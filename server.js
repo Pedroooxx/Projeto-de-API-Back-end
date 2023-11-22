42f9da47dae783fd461864aeb773c76dd4197b79
Pedro de Oliveira Machado <112742316+Pedroooxx@users.noreply.github.com>
2023-11-22 13:08:20 -0300

    Add .gitignore and more

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+require("dotenv").config()
+
 const express = require("express")
 const app = express()
 
+const mongoose = require("mongoose")
+
+const bcrypt = require("bcrypt")
+
+const jwt = require("jsonwebtoken")
 
 app.get('/', (req, res) => {
-    
+    res.status(200).json({msg: "Bem vindo!"})
 })
 
 app.listen(3333)
