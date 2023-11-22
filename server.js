18f58182459136bd36f45dc205bca4f84edcae71
Pedro de Oliveira Machado <112742316+Pedroooxx@users.noreply.github.com>
2023-11-22 11:52:56 -0300

    Add .env

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 const express = require("express")
 const app = express()
 
-const posts = [
-    {
-        username: "Pedro",
-        title: "Post 1"
-    },
-    {
-        username: "Carlos",
-        title: "Post 2"
-    }
-]
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
+app.get('/', (req, res) => {
+    
 })
 
 app.listen(3333)
