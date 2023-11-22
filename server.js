f29d27c048b2269524599555bfae7a93aed4c3e0
Pedro de Oliveira Machado <112742316+Pedroooxx@users.noreply.github.com>
2023-11-22 13:24:38 -0300

    Connecting to MongoDB

--- server.js
+++ server.js
@@ -13,4 +13,16 @@ app.get('/', (req, res) => {
     res.status(200).json({msg: "Bem vindo!"})
 })
 
-app.listen(3333)
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+
+mongoose
+    .connect(
+        `mongodb+srv://${dbUser}:${dbPassword}@cluster0.nbc79ru.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() => {
+    app.listen(3333)
+    console.log("Conectado ao MongoDB")
+    })
+.catch((err) => console.log(err))
+
