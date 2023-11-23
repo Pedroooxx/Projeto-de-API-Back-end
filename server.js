723e7ce18d054e488467af8fd233ef5f49c5e133
Pedro de Oliveira Machado <112742316+Pedroooxx@users.noreply.github.com>
2023-11-23 15:56:51 -0300

    Update

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 require("dotenv").config()
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 const express = require("express")
 const app = express()
-
-const mongoose = require("mongoose")
-
-const bcrypt = require("bcrypt")
-
-const jwt = require("jsonwebtoken")
-
 app.use(express.json())
 
 const User = require("./models/User")
@@ -27,25 +23,6 @@ app.get("/usuario/:id", checkToken, async (req, res) => {
     }
     res.status(200).json({user})
 })
-
-function checkToken(req, res, next) {
-    const authHeader = req.headers["authorization"]
-    const token = authHeader && authHeader.split(" ")[1]
-    
-    if(!token) {
-        return res.status(401).json({msg: "Acesso negado."})
-    }
-
-    try{
-        const secret = process.env.SECRET
-        jwt.verify(token, secret)
-
-        next()
-
-    } catch(error) {
-        res.status(400).json({msg: "Token Invalido."})
-    }
-}
 
 app.post('/registro', async(req, res) => {
 
@@ -117,21 +94,39 @@ app.post('/entrar', async(req, res) => {
         )
         res.status(200).json({msg: "Autenticação bem sucedida", token})
     } catch(err) {
-        console.log(error)
-        
+        console.log(err)
+    }
+})
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers["authorization"]
+    const token = authHeader && authHeader.split(" ")[1]
+    
+    if(!token) {
+        return res.status(401).json({msg: "Acesso negado."})
     }
 
-})
+    try{
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+
+        next()
+
+    } catch(error) {
+        res.status(400).json({msg: "Token Invalido."})
+    }
+}
 
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASSWORD
+const PORT = process.env.PORT
 
 mongoose
     .connect(
         `mongodb+srv://${dbUser}:${dbPassword}@cluster0.nbc79ru.mongodb.net/?retryWrites=true&w=majority`
     )
     .then(() => {
-    app.listen(3333)
+    app.listen(PORT)
     console.log("Conectado ao MongoDB...")
     })
 .catch((err) => console.log(err))
