9131dee76a085b9f72d34344d070f6033eccd890
Pedro de Oliveira Machado <112742316+Pedroooxx@users.noreply.github.com>
2023-11-23 15:06:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,36 @@ app.get('/', (req, res) => {
     res.status(200).json({msg: "Bem vindo!"})
 })
 
+app.get("/usuario/:id", checkToken, async (req, res) => {
+    const id = req.params.id
+
+    const user = await User.findById(id, "-password")
+
+    if(!user) {
+        return res.status(404).json({msg: "Usuario não encontrado."})
+    }
+    res.status(200).json({user})
+})
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers["authorization"]
+    const token = authHeader && authHeader.split(" ")[1]
+    
+    if(!token) {
+        return res.status(401).json({msg: "Acesso negado."})
+    }
+
+    try{
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+
+        next()
+
+    } catch(error) {
+        res.status(400).json({msg: "Token Invalido."})
+    }
+}
+
 app.post('/registro', async(req, res) => {
 
     const {name, password, email} = req.body
@@ -43,7 +73,7 @@ app.post('/registro', async(req, res) => {
     const user = new User({
         name,
         email,
-        password
+        password: passwordHash
     })
     try {
         await user.save()
@@ -52,8 +82,44 @@ app.post('/registro', async(req, res) => {
         console.log(error)
         res.status(500).json({msg: "Erro no Servidor"})
     }
+})
 
+app.post('/entrar', async(req, res) => {
 
+    const {password, email} = req.body
+
+    if(!email) {
+        return res.status(422).json({msg: "Digite o Email."})
+    }
+    if(!password) {
+        return res.status(422).json({msg: "Digite a Senha."})
+    }
+
+    const user = await User.findOne({email: email})
+
+    if(!user) {
+        return res.status(404).json({msg: "Email Invalido"})
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword) {
+        return res.status(404).json({msg: "Senha Invalida"})
+    }
+
+    try {
+        const secret = process.env.SECRET
+        const token = jwt.sign(
+            {
+            id: user._id,
+            },
+            secret,
+        )
+        res.status(200).json({msg: "Autenticação bem sucedida", token})
+    } catch(err) {
+        console.log(error)
+        
+    }
 
 })
 
